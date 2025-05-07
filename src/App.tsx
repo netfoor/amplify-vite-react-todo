f7e8dd5877e69f87f02e1ecb6aef2c3786643baf
Fortino Romero <2311080254@alumno.utpuebla.edu.mx>
2025-05-07 17:03:37 -0600

    style: remove documentation

--- src/App.tsx
+++ src/App.tsx
@@ -43,9 +43,7 @@ function App() {
       <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
+        
       </div>
 
       <button onClick={signOut}>Sign out</button>
